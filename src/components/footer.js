970d4d9e488925a57dabbe52749e16f29ee8c4cb
Prakhar <prakharsingh242@gmail.com>
2021-02-07 19:38:19 +0530

    Feat:Footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -70,6 +70,14 @@ export default function Footer() {
               </div>
             </div>
           </div>
+          <hr className="my-6 border-gray-400" />
+          <div className="flex flex-wrap items-center md:justify-between justify-center">
+            <div className="w-full md:w-4/12 px-4 mx-auto text-center">
+              <div className="text-sm text-gray-600 font-semibold py-1">
+                 Made with <i class="fas fa-bolt"></i> by Webops, EEE Association
+              </div>
+            </div>
+          </div>
         </div>
       </footer>
     </>
